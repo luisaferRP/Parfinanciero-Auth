3d654a913283fd737b0fbf0a0779ee5283ca6546
arlexz96 <arlex.z96@gmail.com>
2025-01-20 11:07:55 -0500

    Feat: Google Auth has been implemented using firebase services

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Kreait\Firebase\Factory;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +12,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton('firebase', function () {
+            $serviceAccount = config('firebase.service_account');
+            return (new Factory)->withServiceAccount($serviceAccount);
+        });
     }
 
     /**
